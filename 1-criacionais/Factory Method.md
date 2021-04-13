3d17c42846a418bfef0c97a9c01f194347a151b6
raloliver <contato@raloliver.com.br>
2021-04-13 22:09:07 +0100

    Simple Factory Demo

--- 1-criacionais/Factory Method.md
+++ 1-criacionais/Factory Method.md
@@ -1,3 +1,4 @@
-# Factory Method #07
+# Factory Method #07 to #10
 
 - after folder: uma fábrica para cada elemento concreto
+- simple folder: se a forma de criar objetos concretos for complexo, é possível exportar eles para novas classes e expor apenas o main Simple Factory
--- 1-criacionais/creational-patterns-end/src/main/java/br/com/cod3r/factory/apple/simple/factory/IPhoneSimpleFactory.java
+++ 1-criacionais/creational-patterns-end/src/main/java/br/com/cod3r/factory/apple/simple/factory/IPhoneSimpleFactory.java
@@ -6,29 +6,34 @@ import br.com.cod3r.factory.apple.simple.model.IPhone11Pro;
 import br.com.cod3r.factory.apple.simple.model.IPhoneX;
 import br.com.cod3r.factory.apple.simple.model.IPhoneXSMax;
 
+/**
+ * This is the more common method
+ */
 public class IPhoneSimpleFactory {
 
 	public static IPhone orderIPhone(String generation, String level) {
 		IPhone device = null;
 
-		if (generation.equals("X")) {
-			if (level.equals("standard")) {
+		if ("X".equals(generation)) {
+			if ("standard".equals(level)) {
 				device = new IPhoneX();
-			} else if (level.equals("highEnd")) {
+			} else if ("highEnd".equals(level)) {
 				device = new IPhoneXSMax();
 			}
 		} else if (generation.equals("11")) {
-			if (level.equals("standard")) {
+			if ("standard".equals(level)) {
 				device = new IPhone11();
-			} else if (level.equals("highEnd")) {
+			} else if ("highEnd".equals(level)) {
 				device = new IPhone11Pro();
 			}
 		}
 
-		device.getHardware();
-		device.assemble();
-		device.certificates();
-		device.pack();
+		if (device != null) {
+			device.getHardware();
+			device.assemble();
+			device.certificates();
+			device.pack();
+		}
 
 		return device;
 	}
