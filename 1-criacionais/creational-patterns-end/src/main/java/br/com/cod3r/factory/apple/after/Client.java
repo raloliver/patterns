0f8040474ce5e7fa875f7fecf1a225bee6ccb377
raloliver <contato@raloliver.com.br>
2021-04-13 22:00:35 +0100

    After FM Demo

--- 1-criacionais/creational-patterns-end/src/main/java/br/com/cod3r/factory/apple/after/Client.java
+++ 1-criacionais/creational-patterns-end/src/main/java/br/com/cod3r/factory/apple/after/Client.java
@@ -4,19 +4,25 @@ import br.com.cod3r.factory.apple.after.factory.IPhone11ProFactory;
 import br.com.cod3r.factory.apple.after.factory.IPhoneFactory;
 import br.com.cod3r.factory.apple.after.factory.IPhoneXFactory;
 import br.com.cod3r.factory.apple.after.model.IPhone;
+import br.com.cod3r.factory.apple.after.factory.IPhone11Factory;
 
 public class Client {
-	
+
 	public static void main(String[] args) {
 		IPhoneFactory iphoneXFactory = new IPhoneXFactory();
 		IPhoneFactory iphone11ProFactory = new IPhone11ProFactory();
-		
+		IPhoneFactory iphone11Factory = new IPhone11Factory();
+
 		System.out.println("### Ordering an iPhone X");
 		IPhone iphone = iphoneXFactory.orderIPhone();
 		System.out.println(iphone);
-		
+
 		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
 		IPhone iphone2 = iphone11ProFactory.orderIPhone();
 		System.out.println(iphone2);
+
+		System.out.println("\n\n### Ordering an iPhone 11");
+		IPhone iphone3 = iphone11Factory.orderIPhone();
+		System.out.println(iphone3);
 	}
 }
--- 1-criacionais/creational-patterns-end/src/main/java/br/com/cod3r/factory/apple/after/factory/IPhoneFactory.java
+++ 1-criacionais/creational-patterns-end/src/main/java/br/com/cod3r/factory/apple/after/factory/IPhoneFactory.java
@@ -2,20 +2,25 @@ package br.com.cod3r.factory.apple.after.factory;
 
 import br.com.cod3r.factory.apple.after.model.IPhone;
 
+/**
+ * 
+ */
 public abstract class IPhoneFactory {
 
 	public IPhone orderIPhone() {
 		IPhone device = null;
-		
+
+		// this is the factory method, that has the responsability to instance a method
 		device = createIPhone();
-		
+
 		device.getHardware();
 		device.assemble();
 		device.certificates();
 		device.pack();
-		
+
 		return device;
 	}
-	
+
+	// any class will extends this one, must have implements this abstract class
 	protected abstract IPhone createIPhone();
 }
