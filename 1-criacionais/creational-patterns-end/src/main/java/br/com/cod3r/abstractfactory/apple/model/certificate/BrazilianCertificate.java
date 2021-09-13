1b434e792ed1da780d87c81ebff231994957d6ff
raloliver <contato@raloliver.com.br>
2021-09-13 06:26:07 +0100

    Starting learning Abstract Factory

--- 1-criacionais/creational-patterns-end/src/main/java/br/com/cod3r/abstractfactory/apple/model/certificate/BrazilianCertificate.java
+++ 1-criacionais/creational-patterns-end/src/main/java/br/com/cod3r/abstractfactory/apple/model/certificate/BrazilianCertificate.java
@@ -3,7 +3,7 @@ package br.com.cod3r.abstractfactory.apple.model.certificate;
 public class BrazilianCertificate implements Certificate {
 
 	public String applyCertification() {
-		return "\t- Calibrating Brasilian rules\n\t- Applying Anatel's Stamp";
+		return "\t- Calibrating Brazilian rules\n\t- Applying Anatel's Stamp";
 	}
 
 }
